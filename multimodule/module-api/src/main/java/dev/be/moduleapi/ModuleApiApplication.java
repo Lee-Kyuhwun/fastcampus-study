5e4113ed4dd3ef1544ddd848dd9a51da51d3890e
이규훈 <dlrbgns0904@sju.ac.kr>
2024-05-06 00:22:09 +0900

    feat: 멀티모듈 모듈간 Bean Test 애너테이션을 통해서 Bean Scan

--- multimodule/module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
+++ multimodule/module-api/src/main/java/dev/be/moduleapi/ModuleApiApplication.java
@@ -1,9 +1,9 @@
-package dev.be; // spring bean 컴포넌트 스캔을 위해 패키지 이동
+package dev.be.moduleapi; // spring bean 컴포넌트 스캔을 위해 패키지 이동
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"dev.be.moduleapi", "dev.be.modulecommon"}) // scanBasePackages 추가
 public class ModuleApiApplication {
 
     public static void main(String[] args) {
