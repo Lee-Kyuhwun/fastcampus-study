5b91abb79d10ed56fe585f5e485f9971c9e4874a
이규훈 <dlrbgns0904@sju.ac.kr>
2024-05-12 19:33:29 +0900

    feat: 로그백 실습

---
+++ logback/src/main/java/dev/be/logback/DemoController.java
@@ -0,0 +1,21 @@
+package dev.be.logback;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class DemoController {
+
+    @GetMapping("/log")
+    public String log() {
+        log.trace("This is a TRACE message");
+        log.debug("This is a DEBUG message");
+        log.info("This is an INFO message");
+        log.warn("This is a WARN message");
+        log.error("This is an ERROR message");
+        return "Check the logs";
+    }
+}
