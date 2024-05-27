8af8742414a3eaf8a06ca09988e43126cf835034
LeeKyuHwun <dlrbgns0904@sju.ac.kr>
2024-05-27 21:33:04 +0900

    feat: git 연습

---
+++ demo-5/src/main/java/dev/be/demo5/Demo5Application.java
@@ -0,0 +1,18 @@
+package dev.be.demo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo5Application {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("args");
+
+
+        SpringApplication.run(Demo5Application.class, args);
+    }
+
+}
