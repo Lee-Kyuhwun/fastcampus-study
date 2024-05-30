6a248b7c888c349e187091bbf9dbe57df989cab8
LeeKyuHwun <dlrbgns0904@sju.ac.kr>
2024-05-30 22:21:36 +0900

    feat: list 실습

---
+++ demo-5/src/main/java/dev/be/demo5/collections/fastcampusclass.java
@@ -0,0 +1,16 @@
+package dev.be.demo5.collections;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class fastcampusclass {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+
+        Set<Integer> set = new HashSet<>();
+
+
+    }
+}
