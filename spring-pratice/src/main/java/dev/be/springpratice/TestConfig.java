fdcba86594c2f907ab21dc8cad0676c99adbf0ff
LeeKyuHwun <dlrbgns0904@sju.ac.kr>
2024-06-02 23:18:31 +0900

    feat: 스프링공부

---
+++ spring-pratice/src/main/java/dev/be/springpratice/TestConfig.java
@@ -0,0 +1,14 @@
+package dev.be.springpratice;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+
+    @Bean
+    public TestService testService() {
+        return new TestService();
+    }
+}
---
+++ spring-pratice/src/main/java/dev/be/springpratice/TestService.java
@@ -0,0 +1,7 @@
+package dev.be.springpratice;
+
+public class TestService {
+    public String getTest() {
+        return "test";
+    }
+}
