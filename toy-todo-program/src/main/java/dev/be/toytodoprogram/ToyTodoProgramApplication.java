1f653facdaf878de3ef1664bc850f8e4098d51f3
LeeKyuHwun <dlrbgns0904@sju.ac.kr>
2024-06-26 20:58:03 +0900

    feat: 일정 관리 프로젝트 생성

---
+++ toy-todo-program/src/main/java/dev/be/toytodoprogram/ToyTodoProgramApplication.java
@@ -0,0 +1,13 @@
+package dev.be.toytodoprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyTodoProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyTodoProgramApplication.class, args);
+    }
+
+}
