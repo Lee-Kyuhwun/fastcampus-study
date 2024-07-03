497ac1b3c9dd841d90ccf80704719aa3a058c729
LeeKyuHwun <dlrbgns0904@sju.ac.kr>
2024-07-03 22:55:46 +0900

    feat: 온라인 커피 주문 시스템 프로젝트 생성

---
+++ online-coffee-order/src/main/java/dev/be/onlinecoffeeorder/OnlineCoffeeOrderApplication.java
@@ -0,0 +1,13 @@
+package dev.be.onlinecoffeeorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCoffeeOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineCoffeeOrderApplication.class, args);
+    }
+
+}
---
+++ online-coffee-order/src/test/java/dev/be/onlinecoffeeorder/OnlineCoffeeOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.be.onlinecoffeeorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCoffeeOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
